cdf89cc8f8829ec9ae10da8fa92dfe02edbea903
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-10-20 19:34:50 -0300

    Update Amplificador.cpp

--- Amplificador.cpp
+++ Amplificador.cpp
@@ -1,2 +1,20 @@
 #include "Amplificador.h"
 
+Amplificador::Amplificador(double ganho): ganho(ganho){
+
+}
+
+Sinal* Amplificador::processar(Sinal* sinalIN){
+    for(int i=0; i<sinalIN->getComprimento(); i++){
+        sinalIN[i] *= ganho;
+    }
+    return sinalIN;
+}
+
+void Amplificador::setGanho(double ganho){
+    this->ganho = ganho;
+}
+
+double Amplificador::getGanho(){
+    return ganho;
+}
