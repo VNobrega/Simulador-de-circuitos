d2827aa85e8960a43f45e62d14758b019deebd98
Nobrega <gjvitor@gmail.com>
2023-10-21 10:52:57 -0300

    Implementacao de alocacao manual de memoria no processamento do amplificador e somador

--- Amplificador.cpp
+++ Amplificador.cpp
@@ -1,19 +1,18 @@
 #include "Amplificador.h"
 
-Amplificador::Amplificador(double ganho): ganho(ganho){
-
-}
-
+Amplificador::Amplificador(double ganho): ganho(ganho){}
 Amplificador::~Amplificador(){}
 
 Sinal* Amplificador::processar(Sinal* sinalIN){
     int comprimento = sinalIN->getComprimento();
-    double sequencia[comprimento];
+    double *sequencia = new double[comprimento];
 
     for(int i = 0; i < comprimento; i++){
         sequencia[i] = sinalIN->getSequencia()[i] * ganho;
     }
     Sinal *sinalA = new Sinal(sequencia,comprimento);
+    delete[] sequencia;
+    
     return sinalA;
 }
 
--- Somador.cpp
+++ Somador.cpp
@@ -1,17 +1,18 @@
 #include "Somador.h"
 
 Somador::Somador(){}
-
 Somador::~Somador(){};
 
 Sinal* Somador::processar(Sinal* sinalIN1, Sinal* sinalIN2){
     int min = (sinalIN1->getComprimento()<sinalIN2->getComprimento())?sinalIN1->getComprimento():sinalIN2->getComprimento();
-    double sequencia[min];
+    double *sequencia = new double[min];
 
     for(int i=0; i<min; i++){
         sequencia[i] = sinalIN1->getSequencia()[i] + sinalIN2->getSequencia()[i];
     }
     
     sinalOUT = new Sinal(sequencia, min);
+    delete[] sequencia;
+
     return sinalOUT;
 }
