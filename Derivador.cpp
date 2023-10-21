5eee014aa8509b48ec7fca822aab2c8b16f79f60
Nobrega <gjvitor@gmail.com>
2023-10-21 17:02:21 -0300

    correcao no primeiro valor do Derivador

--- Derivador.cpp
+++ Derivador.cpp
@@ -6,7 +6,7 @@ Derivador::~Derivador(){}
 Sinal* Derivador::processar(Sinal* sinalIN){
     int comprimento = sinalIN->getComprimento();
     double *sequencia = new double[comprimento];
-    sequencia[0] = 0;
+    sequencia[0] = sinalIN->getSequencia()[0];
     for(int i=1; i<comprimento; i++){
         sequencia[i] = sinalIN->getSequencia()[i]-sinalIN->getSequencia()[i-1];
     }
