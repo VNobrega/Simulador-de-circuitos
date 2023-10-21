1ee6eed35c8924a7ef9f194b4587bc44be9b2b5d
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-10-21 15:00:07 -0300

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Sinal.h"
+#include "ModuloRealimentado.h"
+#define MAX 60
 
 using namespace std;
 
@@ -15,35 +17,54 @@ void menu(){
     << "2) Sua propria sequencia de operacoes" << endl
     << "Escolha: ";
     cin >> escolha1;
-    cout << endl << endl;
+    cout << endl;
 
     if(escolha1 = 1){
+        Sinal* sinal = novoSinal();
 
+        cout << "Qual o ganho do acelerador?" << endl
+        << "g = ";
+        cin >> g;
+        cout << endl;
+
+        ModuloRealimentado* pilotoAutomatico = new ModuloRealimentado(g);
+        (pilotoAutomatico->processar(sinal))->imprimir("Velocidade do Carro");
+    }
+
+    if(escolha1 = 2) {
+
+    }
+}
+
+Sinal* novoSinal(){
+    double sequencia[MAX];
+    int escolha2;
     cout << "Qual sinal voce gostaria de utilizar como entrada da sua simulacao?" << endl
     << "1) 5+3*cos(n*pi/8)" << endl
     << "2) constante" << endl
     << "3) rampa" << endl
     << "Escolha: ";
     cin >> escolha2;
-    cout << endl << endl;
-
-    cout << "Qual o valor dessa constante?" << endl
-    << "C = ";
-    cin >> c;
-    cout << endl << endl;
-
-    cout << "Qual o ganho do acelerador?" << endl
-    << "g = ";
-    cin >> g;
     cout << endl;
 
+    if(escolha2 = 1) {
+
     }
 
-    if(escolha1 = 2) {
-        
+    if(escolha2 = 2) {
+        double c;
+        cout << "Qual o valor dessa constante?" << endl
+        << "C = ";
+        cin >> c;
+        cout << endl;
+
+        for(int i = 0; i < MAX; i++)
+            sequencia[i] = c;
     }
-}
 
-Sinal* novoSinal(){
+    if(escolha2 = 3) {
 
+    }
+    Sinal* sinal = new Sinal(sequencia,MAX);
+    return sinal;
 }
