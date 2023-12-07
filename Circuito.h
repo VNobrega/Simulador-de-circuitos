773401174d74eded7f7ab0ce2e33d7f1086a6f0c
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-12-06 21:22:14 -0300

    Update Circuito.h

--- Circuito.h
+++ Circuito.h
@@ -1,15 +1,21 @@
 #ifndef CIRCUITO_H
 #define CIRCUITO_H
 
+#include <iostream>
+#include<string>
+using namespace std;
+
 class Circuito{
 private:
+    static int ultimoID;
+    int ID;
 
 public:
-Circuito();
-virtual ~Circuito();
-int getID();
-void imprimir();
-static int getUltimoID();
+    Circuito();
+    virtual ~Circuito() = 0;
+    int getID();
+    void imprimir();
+    static int getUltimoID();
 
 };
 
