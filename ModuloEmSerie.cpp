8534cdaae3c9d7db2a3ff8e634b1cfad6cb3fa95
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-12-07 01:14:15 -0300

    Create ModuloEmSerie.cpp

---
+++ ModuloEmSerie.cpp
@@ -0,0 +1,18 @@
+#include "ModuloEmSerie.h"
+
+ModuloEmSerie::ModuloEmSerie():Modulo(){}
+ModuloEmSerie::~ModuloEmSerie(){}
+
+Sinal* ModuloEmSerie::processar(Sinal* sinalIN){
+    if(circuitos->size() == 0)
+        throw new logic_error ("Modulo Vazio");
+    list<CircuitoSISO*>::iterator i = circuitos->begin();
+    Sinal* sinalOUT = sinalIN;
+
+    while(i != circuitos->end()) {
+        sinalOUT = (*i)->processar(sinalOUT);
+        i++;
+    }
+    
+    return sinalOUT;
+}
