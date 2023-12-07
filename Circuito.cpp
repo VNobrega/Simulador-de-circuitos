98c94cef3943fbe9ff2f569232f2d73341cfa2fd
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-12-06 21:22:34 -0300

    Update Circuito.cpp

--- Circuito.cpp
+++ Circuito.cpp
@@ -1,1 +1,22 @@
 #include "Circuito.h"
+
+int Circuito::ultimoID = 0;
+
+Circuito::Circuito(){
+    Circuito::ultimoID++;
+    ID = Circuito::ultimoID;
+}
+
+Circuito::~Circuito(){}
+
+int Circuito::getID(){
+    return ID;
+}
+
+void Circuito::imprimir(){
+    cout << "Circuito com ID [" << Circuito::ID << "]" << endl;
+}
+
+int Circuito::getUltimoID(){
+    return Circuito::ultimoID;
+}
