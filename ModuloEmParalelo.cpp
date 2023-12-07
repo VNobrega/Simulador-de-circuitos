bdb6922be5b35324051e1980e1535dd536b0f15f
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-12-07 01:15:12 -0300

    Create ModuloEmParalelo.cpp

---
+++ ModuloEmParalelo.cpp
@@ -0,0 +1,23 @@
+#include "ModuloEmParalelo.h"
+#include "Somador.h"
+
+ModuloEmParalelo::ModuloEmParalelo():Modulo(){}
+ModuloEmParalelo::~ModuloEmParalelo(){}
+
+Sinal* ModuloEmParalelo::processar(Sinal* sinalIN){
+    if(circuitos->size() == 0)
+        throw new logic_error ("Modulo Vazio");
+    list<CircuitoSISO*>::iterator i = circuitos->begin();
+    Somador* somador = new Somador();
+    Sinal* sinalOUT = (*i)->processar(sinalIN);
+    i++;
+
+    while(i != circuitos->end()) {
+        sinalOUT = somador->processar(sinalOUT, (*i)->processar(sinalIN));
+        i++;
+    }
+
+    somador->~Somador();
+    
+    return sinalOUT;
+}
