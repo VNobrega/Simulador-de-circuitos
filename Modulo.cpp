6ae6b9fb31391e0cb761befc4d1933036bb3dc65
gtutiya <148587597+gtutiya@users.noreply.github.com>
2023-12-06 23:38:14 -0300

    Create Modulo.cpp

---
+++ Modulo.cpp
@@ -0,0 +1,28 @@
+#include "Modulo.h"
+
+Modulo::Modulo(){
+    list<CircuitoSISO*>* circuitos = new list<CircuitoSISO*>();
+}
+
+Modulo::~Modulo(){
+    delete circuitos;
+}
+
+void Modulo::adicionar(CircuitoSISO* circ){
+    circuitos->push_back(circ);
+}
+
+list<CircuitoSISO*>* Modulo::getCircuitos(){
+    return circuitos;
+}
+
+void Modulo::imprimir(){
+    list<CircuitoSISO*>::iterator i = circuitos->begin();
+
+    cout << "Modulo com ID [" << getID() << "] e:" << endl;
+    while(i != circuitos->end()) {
+        cout << "Circuito com ID [" << (*i)->getID() << "]" << endl;
+        i++;
+    }
+    cout << "--" << endl;
+}
