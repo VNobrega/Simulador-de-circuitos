ea1c8d2f6cfa480c8a8ec57af1e7986ae88893b2
Nobrega <gjvitor@gmail.com>
2023-12-08 19:08:18 -0300

    Leitura de arquivos

--- PersistenciaDeModulo.cpp
+++ PersistenciaDeModulo.cpp
@@ -1,4 +1,8 @@
 #include "PersistenciaDeModulo.h"
+#include "ModuloEmSerie.h"
+#include "ModuloEmParalelo.h"
+#include "ModuloRealimentado.h"
+
 
 void saidaArquivo(Modulo* mod,ofstream& output);
 
@@ -16,42 +20,29 @@ void PersistenciaDeModulo::salvarEmArquivo(Modulo* mod){
     output.close();
 }
 
-Modulo* PersistenciaDeModulo::lerDeArquivo(){
-    ifstream input;
-    input.open(nomeDoArquivo);
-
-    if(input.fail()) {
-        input.close();
-        throw new invalid_argument ("Arquivo nao encontrado");
-    }
-
-    Modulo* moduloLido = entradaArquivo(input);
-
-    if(!input.eof()){
-        input.close();
-        throw new logic_error ("Erro de leitura");
-    }
-    
-    input.close();
-
-    return moduloLido;
-}
 
 void saidaArquivo(Modulo* mod,ofstream& output){
-    list<CircuitoSISO*>* circuitos = mod->getCircuitos();
-    list<CircuitoSISO*>::iterator i = circuitos->begin();
+    list<CircuitoSISO*>* circuitos = new list<CircuitoSISO*>;
 
     ModuloEmSerie* ms = dynamic_cast<ModuloEmSerie*>(mod);
-    if(ms != NULL)
+    if(ms != NULL){
         output << "S" << endl;
+        circuitos = ms->getCircuitos();
+    }
 
     ModuloEmParalelo* mp = dynamic_cast<ModuloEmParalelo*>(mod);
-    if(mp != NULL)
+    if(mp != NULL){
         output << "P" << endl;
-        
+        circuitos = mp->getCircuitos();
+    }
+
     ModuloRealimentado* mr = dynamic_cast<ModuloRealimentado*>(mod);
-    if(mr != NULL)
+    if(mr != NULL){
         output << "R" << endl;
+        circuitos = mr->getCircuitos();
+    }
+
+    list<CircuitoSISO*>::iterator i = circuitos->begin();
 
     while(i != circuitos->end()) {
         ModuloEmSerie* s = dynamic_cast<ModuloEmSerie*>(*i);
@@ -84,11 +75,61 @@ void saidaArquivo(Modulo* mod,ofstream& output){
 
     delete circuitos;
 }
-    output << "f" << endl;
 
-    delete circuitos;
+Modulo* PersistenciaDeModulo::lerDeArquivo(){
+    ifstream input;
+    input.open(nomeDoArquivo);
+
+    if(input.fail()) {
+        input.close();
+        throw new invalid_argument ("Arquivo nao encontrado");
+    }
+
+    Modulo* moduloLido = entradaArquivo(input);
+
+    if(!input.eof()){
+        input.close();
+        moduloLido->~Modulo();
+        throw new logic_error ("Erro de leitura");
+    }
+    
+    input.close();
+
+    return moduloLido;
 }
+    
 
 Modulo* entradaArquivo(ifstream& input){
+    Modulo* moduloLer = nullptr;
+    char c;
+    input>>c;
+    if(c == 'S') moduloLer = new ModuloEmSerie();
+    if(c == 'P') moduloLer = new ModuloEmParalelo();
+    if(c == 'R') moduloLer = new ModuloRealimentado();
+    input>>c;
+    while(c != 'f'){
+       
+        if(c == 'S' || c == 'P' || c == 'R') moduloLer->adicionar(entradaArquivo(input));
+        else if(c == 'I'){
+            CircuitoSISO *circ = new Integrador();
+            moduloLer->adicionar(circ);
+        }
+        else if(c == 'D'){
+            CircuitoSISO *circ = new Derivador();
+            moduloLer->adicionar(circ);
+        }
+        else if(c == 'A'){
+            double d;
+            if(!(input>>d)) return moduloLer;
+            CircuitoSISO *circ = new Amplificador(d);
+            moduloLer->adicionar(circ);
+        } 
+       
+        else return moduloLer;
+        input>>c;
+    }
+
+    input>>c;
+    return moduloLer; 
 
 }
